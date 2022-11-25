8cecfca835230e02a614447b01452aed1f074ef6
Andrii Korutchak <andrii.korutchak@gmail.com>
2022-11-25 10:32:44 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import lesson_1.HelloWorld;
 public class Main {
     public static void main(String[] args) {
         //lesson_1
+        System.out.println("Hello, World!");
+
         HelloWorld hw = new HelloWorld();
         hw.printHelloWorld();
 
