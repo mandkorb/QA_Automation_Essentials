bb5edbc6535b1c37d1a63ce17c86e45f86eed8fa
mandkorb <mandkorb@gmail.com>
2025-01-06 12:32:38 +0200

    changed project structure

--- src/main/java/lesson_1/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package lesson_1;
-
-public class HelloWorld {
-    public void printHelloWorld(){
-        System.out.println("Hello, World!");
-    }
-}
--- src/main/java/lesson_2/GitEssentials.java
+++
@@ -1,6 +0,0 @@
-package lesson_2;
-
-public class GitEssentials {
-    //git config --global user.name "Andrii Korutchak"
-    //git config --global user.email andrii.korutchak@gmail.com
-}
