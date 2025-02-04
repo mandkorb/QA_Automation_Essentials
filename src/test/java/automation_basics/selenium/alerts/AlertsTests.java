2807936db891113756c0454ebe7553c563ab8f5a
mandkorb <mandkorb@gmail.com>
2025-02-04 18:56:54 +0200

    added alert wait

--- src/test/java/automation_basics/selenium/alerts/AlertsTests.java
+++ src/test/java/automation_basics/selenium/alerts/AlertsTests.java
@@ -5,19 +5,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import java.time.Duration;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 public class AlertsTests {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeClass
     public void setupDriver() {
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.manage().window().maximize();
     }
 
@@ -37,7 +42,7 @@ public class AlertsTests {
     public void clickJSAlert() {
         clickOnButtonByText("Click for JS Alert");
 
-        Alert alert = driver.switchTo().alert();
+        Alert alert = wait.until(alertIsPresent());
         Assert.assertEquals(alert.getText(), "I am a JS Alert");
         alert.accept();
 
@@ -48,7 +53,7 @@ public class AlertsTests {
     @Test
     public void clickJSAlertConfirm() {
         clickOnButtonByText("Click for JS Confirm");
-        Alert alert = driver.switchTo().alert();
+        Alert alert = wait.until(alertIsPresent());
         Assert.assertEquals(alert.getText(), "I am a JS Confirm");
 
         alert.dismiss();
@@ -66,7 +71,7 @@ public class AlertsTests {
         String promptMessage = "test";
 
         clickOnButtonByText("Click for JS Prompt");
-        Alert alert = driver.switchTo().alert();
+        Alert alert = wait.until(alertIsPresent());
         alert.dismiss();
         WebElement resultMessage = driver.findElement(By.id("result"));
         Assert.assertEquals(resultMessage.getText(), "You entered: null");
